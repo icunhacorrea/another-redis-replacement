fdea0d1bb7c38ad4cfe3314c4365a7844035db2c
Iago Corrêa <icorrea@Iagos-MacBook-Air.local>
2024-06-17 21:17:13 -0300

    Cleaing project.

--- arr/exceptions/type_not_found_ex.py
+++
@@ -1,6 +0,0 @@
-
-class TypeNotFoundException(Exception):
-
-    def __init__(self, *args: object) -> None:
-        super(TypeNotFoundException, self).__init__(*args)
-
--- arr/utils/breader.py
+++
@@ -1,5 +0,0 @@
-
-class ByteReader:
-    def first_byte(self, data: bytes) -> bytes:
-        return data[0:1]
-
